8b186f636d9a1c2475923f49d24eb3deb778f8f8
amir mohammad ghanbari <ghanbari-a>
2023-09-13 22:54:28 +0330

    final

---
+++ DOC.md
@@ -0,0 +1,12 @@
+#Edit the values.yaml file in charts/values.yaml according to your needs:
+```
+vim charts/values.yaml
+```
+
+chmod +x client-setup.sh; ./client-setup.sh
+
+
+
+
+
+
--- client-setup.sh
+++ client-setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 IPS=$1
 USER=$2
+PAM_PASS=$3
+SUDO_PASS=$4
+PKEY=~/.ssh/id_rsa
 DIST=$(cat /etc/lsb-release | grep DISTRIB_ID | cut -d '=' -f 2)
 if [ $DIST != "Ubuntu" ]; then
 	echo "Please switch to ubuntu :)"
 	exit
 fi
+
+WP=$(cat charts/values.yaml | grep url | cut -d '"' -f 2 | head -n 1)
+PMA=$(cat charts/values.yaml | grep url | cut -d '"' -f 2 | tail -n 1)
+USERNAME=$(cat charts/values.yaml | grep username | cut -d ':' -f 2 | tail -n 1)
+PASSWORD=$(cat charts/values.yaml | grep password | cut -d ':' -f 2 | tail -n 1)
 
 apt update
 apt install python3-pip python3-venv git sshpass -y
@@ -23,11 +31,26 @@ cd $KUBESPRAYDIR
 pip install -U -r requirements.txt
 
 
-
 declare -a IPS="($IPS)"
 
 CONFIG_FILE=inventory/sample/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
 
 
-ansible-playbook -i inventory/sample/hosts.yaml  --become --become-user=root cluster.yml -kK -u $USER
-ansible-playbook -i kubespray/inventory/sample/hosts.yaml plays/post_cluster.yaml  -kK -u $USER
+ansible-playbook -i inventory/sample/hosts.yaml --extra-vars "ansible_ssh_user=$USER ansible_ssh_pass=$PAM_PASS ansible_sudo_pass=$SUDO_PASS"  --become --become-user=root cluster.yml
+cd ..
+ansible-playbook -i kubespray/inventory/sample/hosts.yaml --extra-vars "ansible_ssh_user=$USER ansible_ssh_pass=$PAM_PASS ansible_sudo_pass=$SUDO_PASS"  plays/post_cluster.yaml
+
+
+
+echo "Navigate to https://$WP for the Wordpress website"
+echo "Navigate to https://$PMA for the Phpmyadmin panel"
+
+
+echo "Database username:$USERNAME"
+echo "Database password:$PASSWORD"
+
+
+
+
+
+
