434c0ce6c08c7d6ffb38e86f2b1c2f2e793c9200
Your Name <you@example.com>
2023-09-14 16:12:26 +0330

    non-root execution fix

--- DOC.md
+++ DOC.md
@@ -2,9 +2,12 @@
 ```
 vim charts/values.yaml
 ```
-
+```#Install these package
+apt update && apt install python3-pip python3-venv git sshpass -y
+```
+```
 chmod +x client-setup.sh; ./client-setup.sh <server-ip> <ssh-password> <sudo-password>
-
+```
 
 
 
--- client-setup.sh
+++ client-setup.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-IPS=$1
+IP=$1
 USER=$2
 PAM_PASS=$3
 SUDO_PASS=$4
-PKEY=~/.ssh/id_rsa
-DIST=$(cat /etc/lsb-release | grep DISTRIB_ID | cut -d '=' -f 2)
-if [ $DIST != "Ubuntu" ]; then
-	echo "Please switch to ubuntu :)"
-	exit
-fi
+PRIVATE_IP=$5
+
+
+
+#Works on Debian as well
+#DIST=$(cat /etc/lsb-release | grep DISTRIB_ID | cut -d '=' -f 2)
+#if [ $DIST != "Ubuntu" ]; then
+#        echo "Please switch to ubuntu :)"
+#        exit
+#fi
 
 WP=$(cat charts/values.yaml | grep url | cut -d '"' -f 2 | head -n 1)
 PMA=$(cat charts/values.yaml | grep url | cut -d '"' -f 2 | tail -n 1)
 USERNAME=$(cat charts/values.yaml | grep username | cut -d ':' -f 2 | tail -n 1)
 PASSWORD=$(cat charts/values.yaml | grep password | cut -d ':' -f 2 | tail -n 1)
 
-apt update
-apt install python3-pip python3-venv git sshpass -y
+#Non-root execution of the script
+#apt update
+#apt install python3-pip python3-venv git sshpass -y
 
 
 git clone https://github.com/kubernetes-sigs/kubespray
@@ -31,10 +36,12 @@ cd $KUBESPRAYDIR
 pip install -U -r requirements.txt
 
 
-declare -a IPS="($IPS)"
+declare -a IPS="($PRIVATE_IP,$IP)"
+
 
 CONFIG_FILE=inventory/sample/hosts.yaml python3 contrib/inventory_builder/inventory.py ${IPS[@]}
 
+sed -i "s/access_ip: $IP/access_ip: $PRIVATE_IP/g" inventory/sample/hosts.yaml
 
 ansible-playbook -i inventory/sample/hosts.yaml --extra-vars "ansible_ssh_user=$USER ansible_ssh_pass=$PAM_PASS ansible_sudo_pass=$SUDO_PASS"  --become --become-user=root cluster.yml
 cd ..
@@ -48,9 +55,3 @@ echo "Navigate to https://$PMA for the Phpmyadmin panel"
 
 echo "Database username:$USERNAME"
 echo "Database password:$PASSWORD"
-
-
-
-
-
-
